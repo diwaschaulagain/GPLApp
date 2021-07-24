5ebdfe7e9a38f4a2981d8c34692da3072d0039c4
Diwas <chaulagaindiwas@yahoo.com>
2021-07-24 19:56:01 +0545

    init commit

--- GPLUnitTesting/CircleTest.cs
+++ GPLUnitTesting/CircleTest.cs
@@ -3,19 +3,19 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace GPLUnitTesting
 {
-    [TestClass]
-    public class CircleTest
-    {
-        /// <summary>
-        /// Method to test the circle while providing manual parameters which checks it with our main program
-        /// </summary>
-        [TestMethod]
-        public void TestCircle()
-        {
-            var r = new Circle();
-            int x = 100, y = 50, radius = 40;
-            r.Set(x, y, radius);
-            Assert.AreEqual(100, r.x); //Assert.AreEqual(expected,actual)
-        }
-    }
+    //[TestClass]
+    //public class CircleTest
+    //{
+    //    /// <summary>
+    //    /// Method to test the circle while providing manual parameters which checks it with our main program
+    //    /// </summary>
+    //    [TestMethod]
+    //    public void TestCircle()
+    //    {
+    //        var r = new Circle();
+    //        int x = 100, y = 50, radius = 40;
+    //        r.Set(x, y, radius);
+    //        Assert.AreEqual(100, r.x); //Assert.AreEqual(expected,actual)
+    //    }
+    //}
 }
